7e6a12934a91f77732e8f97b6811d2afd1df92c5
Sourav Mondal <sourav.mondal@beas.co.in>
2023-09-05 19:21:01 +0530

    first commit

---
+++ components/user_header.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="home.php" class="logo">Blogo.</a>
+
+  
+
+    
+
+
+   </section>
+
+</header>
